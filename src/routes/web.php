1b81893d16140c2fde81d4f254751cab9ec2ffa0
kentakki416 <fujiken0814@outlook.jp>
2021-10-02 12:34:18 +0900

    pull&request

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IndexController;
 
 use App\Http\Controllers\UsersController;
-
+//プル陸
 
 /*
 |--------------------------------------------------------------------------

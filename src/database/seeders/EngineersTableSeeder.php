70d3dc03d163f8aaf947fa609587bc1ff580c746
schnell3526 <schnelldickheiter@gmail.com>
2021-10-10 19:37:33 +0900

    seederを修正

--- src/database/seeders/EngineersTableSeeder.php
+++ src/database/seeders/EngineersTableSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class EngineerTableSeeder extends Seeder
+class EngineersTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.

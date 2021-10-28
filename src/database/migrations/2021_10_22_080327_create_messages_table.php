db6cf4d46e9eb09a7cc76a880a6d2f99c54d5181
naoki183 <yamachan882@gmail.com>
2021-10-28 23:18:20 +0900

    通知機能，メール送信機能

--- src/database/migrations/2021_10_22_080327_create_messages_table.php
+++ src/database/migrations/2021_10_22_080327_create_messages_table.php
@@ -18,6 +18,7 @@ class CreateMessagesTable extends Migration
             $table->integer('send_user_id');
             $table->integer('receive_user_id');
             $table->text('message');
+            $table->integer('checked')->default(0);
             $table->timestamps();
         });
     }
